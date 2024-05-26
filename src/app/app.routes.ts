c137a141d5beb2360bcbdd35001496dccacdae3c
hamstermaniac <lupu_andrei_la@yahoo.com>
2024-05-26 18:59:49 +0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { ConsentComponent } from './components/consent/consent.component';
-
-export const routes: Routes = [
-    { path: '', component: ConsentComponent },
-];
